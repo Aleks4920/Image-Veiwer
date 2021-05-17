cc909adaf58f0ffa72d63c318217970cb77d5fe4
Aleks4920 <49626050+Aleks4920@users.noreply.github.com>
2021-05-17 16:22:57 -0400

    Add files via upload

---
+++ img_veiwer.py
@@ -0,0 +1,67 @@
+
+
+import PySimpleGUI as sg
+import os.path
+
+file_list_column = [
+    [
+        sg.Text("Image Folder"),
+        sg.In(size=(25,1), enable_events=True, key="-FOLDER-"),
+        sg.FolderBrowse()
+    ],
+    [
+        sg.Listbox(
+            values=[], enable_events=True,size=(40,20), key="-FILE LIST-"
+        )
+    ]
+]
+
+image_veiwer_column = [
+    [sg.Text("choose and Image")],
+    [sg.Text(size=(40,10), key="-TOUT-")],
+    [sg.Image(key="-IMAGE-")]
+]
+
+layout = [
+    [
+        sg.Column(file_list_column),
+        sg.VSeperator(),
+        sg.Column(image_veiwer_column)
+    ]
+]
+
+window= sg.Window("Image Veiwer", layout)
+
+while True:
+    event, values = window.read()
+    if event =="Exit" or event == sg.WIN_CLOSED:
+        break
+    if event == "-FOLDER-":
+        folder = values["-FOLDER-"]
+        try:
+            file_list = os.listdir(folder)
+        except:
+            file_list =[]
+
+        fnames = [
+        f
+        for f in file_list
+        if os.path.isfile(os.path.join(folder, f))
+        and f.lower().endswith((".png",".gif"))
+
+        ]
+        window["-FILE LIST-"].update(fnames)
+    elif event == "-FILE LIST-":
+        try:
+            filename=os.path.join(values["-FOLDER-"], values["-FILE LIST-"][0])
+
+            window["-TOUT-"].update(filename)
+
+            window["-IMAGE-"].update(filename=filename)
+            print(window["-IMAGE-"].get_size())
+
+
+        except:
+            pass
+
+window.close()
